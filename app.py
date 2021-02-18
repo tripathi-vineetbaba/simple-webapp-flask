033673336c48784acf4fec9ac66d9fb82689234f
Vineet Tripathi <tripathi.vineetbaba@gmail.com>
2021-02-18 12:14:01 +0530

    Added Config

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
 app = Flask(__name__)
+import yaml
+with open('config.yml') as data:
+     config=yaml.safe_load(data)
+
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return ("Welcome to "+ config['env'] +" !")
 
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=9898)
